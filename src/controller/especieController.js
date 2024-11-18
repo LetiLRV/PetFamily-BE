fb41c21facb901ba5143629e22f6f77f5bc12d60
Letícia Lopes <leticialopesrv@gmail.com>
2024-11-18 11:30:09 -0300

    Funções da tabela Especie
    amém

--- src/controller/especieController.js
+++ src/controller/especieController.js
@@ -10,14 +10,79 @@ async function getEspecies(req,res){
         res.status(200).send(result)
 
     } catch (error) {
-        res.status(500).send('Erro ao ler as especies, verifique o console')
+        res.status(500).send('Erro ao ler as espécies, verifique o console')
         console.log(error)
 
     }
 }
 
+async function insertEspecie(req,res) {
+    try{
+        const sql = await sqlconnection()
+
+        const {descricao} = req.body
+
+        await sql.query('call InsertEspecie(?)',[descricao])
+
+        res.status(201).json({
+            "menssage": "Espécie insirada com sucesso",
+            descricao
+        })
+
+    } catch(error){
+        res.status(500).send('Erro ao inserir a espécie, verifique o console')
+        console.log(error)
+
+    }   
+}
+
+async function updateEspecie(req,res) {
+    try{
+        const sql = await sqlconnection()
+
+        const {idEspecie} = req.params
+        const {descricao} = req.body
+
+        await sql.query('call UpdateEspecie(?,?)',[idEspecie, descricao])
+
+        res.status(200).json({
+            "menssage": "Espécie alterada com sucesso",
+            idEspecie, 
+            descricao
+        })
+        
+    } catch(error){
+        res.status(500).send('Erro ao alterar a espécie, verifique o console')
+        console.log(error)
+    }
+    
+}
+
+async function deleteEspecie(req,res) {
+    try{
+        const sql = await sqlconnection()
+
+        const {idEspecie} = req.params
+
+        await sql.query('call DeleteEspecie(?)',[idEspecie])
+
+        res.status(200).json({
+            "menssage": "Espécie apagada com sucesso",
+            idEspecie
+        })
+
+    } catch(error){
+        res.status(500).send('Erro ao apagar a espécie, verifique o console')
+        console.log(error)
+    }
+
+}
+
 
 module.exports = {
     getEspecies,
+    insertEspecie,
+    updateEspecie,
+    deleteEspecie
     
 }
--- src/route/especieRoute.js
+++ src/route/especieRoute.js
@@ -4,5 +4,11 @@ const especieController = require('../controller/especieController.js')
 
 router.get('/especie',especieController.getEspecies)
 
+router.post('/especie',especieController.insertEspecie)
+
+router.put('/especie/:idEspecie', especieController.updateEspecie)
+
+router.delete('/especie/:idEspecie', especieController.deleteEspecie)
+
 
 module.exports = router
